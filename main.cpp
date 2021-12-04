36fb0cac63ecd1898dc9164b231adbe330e06f63
Shawn Halayka <sjhalayka@gmail.com>
2021-12-03 22:42:43 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,151 @@
+#include "main.h"
+
+
+
+
+
+int main(void)
+{
+	vector<pair<float, float>> thresholds;
+
+	vector<vector<triangle>> triangles;
+
+	pair<float, float> f;
+
+	f.first = 4.0f;
+	f.second = 0.0f;
+	thresholds.push_back(f);
+
+	//f.first = 1.95f;
+	//f.second = 0.55f;
+	//thresholds.push_back(f);
+
+	//f.first = 0.5f;
+	//f.second = 0.0f;
+	//thresholds.push_back(f);
+
+	triangles.resize(thresholds.size());
+
+	const float grid_max = 1.5;
+	const float grid_min = -grid_max;
+	const size_t res = 100;
+
+	const bool make_border = true;
+
+	const unsigned short int max_iterations = 8;
+	const float threshold = 4;
+
+	// When adding a border, use a value that is "much" greater than the threshold.
+	const float border_value = 1.0f + threshold;
+
+
+	vector<float> xyplane0(res * res, 0);
+	vector<float> xyplane1(res * res, 0);
+
+	const float step_size = (grid_max - grid_min) / (res - 1);
+
+	octonion C;
+	C.vertex_data[0] = 0.3f;
+	C.vertex_data[1] = 0.5f;
+	C.vertex_data[2] = 0.4f;
+	C.vertex_data[3] = 0.2f;
+	C.vertex_data[4] = 0.1f;
+	C.vertex_data[5] = 0.0f;
+	C.vertex_data[6] = 0.0f;
+	C.vertex_data[7] = 0.0f;
+
+	octonion Z;
+
+	for (size_t i = 0; i < 3; i++)
+		Z.vertex_data[i] = grid_min;
+
+	// Do slice of 8D set
+	const float slice_val = 0.6f;
+
+	size_t z = 0;
+
+	// Calculate first xy plane
+	for (size_t x = 0; x < res; x++, Z.vertex_data[0] += step_size)
+	{
+		Z.vertex_data[1] = grid_min;
+
+		for (size_t y = 0; y < res; y++, Z.vertex_data[1] += step_size)
+		{
+//			if (z > res / 2)
+//				xyplane0[x * res + y] = border_value;
+//			else
+//			{
+				if (true == make_border && (x == 0 || y == 0 || z == 0 || x == res - 1 || y == res - 1 || z == res - 1))
+					xyplane0[x * res + y] = border_value;
+				else
+					xyplane0[x * res + y] = iterate(Z, C, slice_val, max_iterations, threshold);
+//			}
+		}
+	}
+
+	// Prepare for 2nd xy plane
+	z++;
+	Z.vertex_data[2] += step_size;
+
+	// Calculate 2nd and subsequent xy planes
+	for (; z < res; z++, Z.vertex_data[2] += step_size)
+	{
+		Z.vertex_data[0] = grid_min;
+
+		cout << "Calculating triangles from xy-plane pair " << z << " of " << res - 1 << endl;
+
+		for (size_t x = 0; x < res; x++, Z.vertex_data[0] += step_size)
+		{
+			Z.vertex_data[1] = grid_min;
+
+			for (size_t y = 0; y < res; y++, Z.vertex_data[1] += step_size)
+			{
+
+//				if (z > res / 2)
+//					xyplane1[x * res + y] = border_value;
+//				else
+//				{
+					if (true == make_border && (x == 0 || y == 0 || z == 0 || x == res - 1 || y == res - 1 || z == res - 1))
+						xyplane1[x * res + y] = border_value;
+					else
+						xyplane1[x * res + y] = iterate(Z, C, slice_val, max_iterations, threshold);
+//				}
+			}
+		}
+
+
+
+		for (size_t t = 0; t < thresholds.size(); t++)
+		{
+			// Calculate triangles for the xy-planes corresponding to z - 1 and z by marching cubes
+			tesselate_adjacent_xy_plane_pair(
+				C, slice_val,
+				threshold,
+				thresholds[t].first,
+				thresholds[t].second,
+				max_iterations,
+				xyplane0, xyplane1,
+				z - 1,
+				triangles[t],
+				grid_min, grid_max, res,
+				grid_min, grid_max, res,
+				grid_min, grid_max, res);
+		}
+
+		// Swap memory pointers (fast) instead of performing a memory copy (slow)
+		xyplane1.swap(xyplane0);
+	}
+
+	for (size_t t = 0; t < triangles.size(); t++)
+	{
+		ostringstream oss;
+		oss << t;
+
+		string filename = "out" + oss.str() + ".stl";
+
+		if (0 < triangles[t].size())
+			write_triangles_to_binary_stereo_lithography_file(triangles[t], filename.c_str());
+	}
+
+	return 0;
+}
---
+++ marching_cubes.h
@@ -0,0 +1,147 @@
+// Modified from Paul Bourke, Polygonising a Scalar Field
+// Source code by Shawn Halayka
+// Source code is in the public domain
+
+
+#ifndef MARCHING_CUBES_H
+#define MARCHING_CUBES_H
+
+
+#include "primitives.h"
+
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <fstream>
+using std::ofstream;
+
+#include <iomanip>
+using std::ios_base;
+
+#include <vector>
+using std::vector;
+
+#include <set>
+using std::set;
+
+#include <cmath>
+using std::sin;
+using std::exp;
+
+
+
+namespace marching_cubes
+{
+	class grid_cube
+	{
+	public:
+		vertex_3 vertex[8];
+		float value[8];
+	};
+
+
+	class octonion
+	{
+	public:
+		inline octonion(void)
+		{
+			for (size_t i = 0; i < 8; i++)
+				vertex_data[i] = 0;
+		}
+
+		float magnitude(void)
+		{
+			float result = 0;
+
+			for (size_t i = 0; i < 8; i++)
+				result += vertex_data[i] * vertex_data[i];
+
+			return sqrtf(result);
+		}
+
+
+		octonion operator+(const octonion& rhs)
+		{
+			octonion result;
+
+			for (size_t i = 0; i < 8; i++)
+				result.vertex_data[i] = vertex_data[i] + rhs.vertex_data[i];
+
+			return result;
+		}
+
+		inline octonion(
+			float src_r,
+			float src_i,
+			float src_j,
+			float src_k,
+			float src_u1,
+			float src_i1,
+			float src_j1,
+			float src_k1)
+		{
+			vertex_data[0] = src_r;
+			vertex_data[1] = src_i;
+			vertex_data[2] = src_j;
+			vertex_data[3] = src_k;
+			vertex_data[4] = src_u1;
+			vertex_data[5] = src_i1;
+			vertex_data[6] = src_j1;
+			vertex_data[7] = src_k1;
+		}
+
+		float vertex_data[8];
+	};
+
+	octonion mul(const octonion& qA, const octonion& qB);
+
+
+	vertex_3 vertex_interp(const float isovalue, vertex_3 p1, vertex_3 p2, float valp1, float valp2);
+	short unsigned int tesselate_grid_cube(octonion C, float z_w, const float isovalue, const float upper_threshold, const float lower_threshold, short unsigned int max_iterations, const grid_cube& grid, triangle* const triangles);
+	void tesselate_adjacent_xy_plane_pair(octonion C, float z_w, const float isovalue, const float upper_threshold, const float lower_threshold, short unsigned int max_iterations, vector<float> xyplane0, vector<float> xyplane1, const size_t z, vector<triangle> &triangles, const float x_grid_min, const float x_grid_max, const size_t x_res, const float y_grid_min, const float y_grid_max, const size_t y_res, const float z_grid_min, const float z_grid_max, const size_t z_res);
+
+
+	octonion sin(const octonion& in);
+
+
+
+	quintonion sin(const quintonion& in);
+
+	quintonion exp(const quintonion& in);
+
+	quintonion ln(const quintonion& in);
+
+	quaternion traditional_mul(const quaternion& in_a, const quaternion& in_b);
+
+	quintonion mul(const quintonion& in_a, const quintonion& in_b);
+
+
+	quintonion conj_number_type(quintonion& in);
+
+	quintonion pow_number_type(quintonion& in, float exponent);
+
+
+
+
+	inline float iterate(
+		octonion Z,
+		octonion C,
+		float z_w,
+		const short unsigned int max_iterations,
+		const float threshold);
+
+	vertex_3 vertex_interp_refine(
+		octonion C,
+		float z_w,
+		float isovalue,
+		float upper_threshold,
+		float lower_threshold,
+		short unsigned int max_iterations,
+		vertex_3 v0, vertex_3 v1,
+		float val_v0, float val_v1);
+
+};
+
+#endif
